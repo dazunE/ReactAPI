9676cb99229465a6eb5474573dcc1133a3052fa1
Dasun Edirisinghe <dazunj4me@gmail.com>
2016-12-03 17:48:14 +0530

    inital commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,30 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?&appid=7475ac794fc8c53f1e7d22e2021d5ea7&units=metric';
+
+module.exports = {
+
+  getTemp : function ( location ){
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function( res ){
+
+      if(res.data.cod && res.data.message){
+
+        throw new Error(res.data.message);
+
+      } else {
+
+        return res.data.main.temp;
+
+      }
+
+    }, function( res ){
+
+      throw new Error(res.data.message);
+
+    });
+
+  }
+}
